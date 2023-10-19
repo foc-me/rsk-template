b6d7b280ffd287bb61d5525d8d8f8753bf62068d
lss <lss.rp@tom.com>
2023-10-19 11:29:03 +0800

    fix: 添加 style script 标签对 content 的支持

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,11 @@
 import type { ReactNode } from "react"
 
 type Link = { href: string, rel?: string }
+type Script = { src?: string, content?: string }
 
 export type Props = Partial<{
     title: string,
-    scripts: string[],
+    scripts: Script[],
     links: Link[],
     styles: string[],
     children: ReactNode
@@ -23,20 +24,23 @@ function App(props: Props) {
         <head>
             <title>{title}</title>
             <meta charSet="UTF-8"/>
-            {scripts.map((script, index) => {
-                return <script key={index} defer type="text/javascript" src={script}/>
-            })}
             {links.map(({ href, rel = "stylesheet" }, index) => {
                 return <link key={index} href={href} rel={rel}/>
             })}
             {styles.map((style, index) => {
-                return <style key={index} type="text/css">
+                const innerHtml = { __html: style }
+                return <style key={index} type="text/css" dangerouslySetInnerHTML={innerHtml}>
                     {style}
                 </style>
             })}
         </head>
         <body>
             <div className="root" id="root">{children}</div>
+            {scripts.map((script, index) => {
+                const { src, content } = script
+                const innerHtml = content ? { __html: content } : undefined
+                return <script key={index} defer type="text/javascript" src={src} dangerouslySetInnerHTML={innerHtml}/>
+            })}
         </body>
     </html>
 }
